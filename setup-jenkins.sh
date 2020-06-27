3fd8a3b7e5d953a4eaa440636dc3ba78591fe9f3
codesenju <codesenju@gmail.com>
2020-06-27 09:38:58 +0200

    working script

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -2,14 +2,14 @@
 
 mkdir -pv $PWD/jenkins-docker-certs
 mkdir -pv $PWD/jenkins-data
-
 docker rm -f jenkins-docker
 docker container run --name jenkins-docker --rm --detach \
   --privileged --network jenkins --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
   --volume "$PWD"/jenkins-docker-certs:/certs/client \
-  --volume "$PWD/"jenkins-data:/var/jenkins_home \
-  --publish 2376:2376 docker:dind
+  --volume "$PWD"/jenkins-data:/var/jenkins_home \
+   -v "$PWD":/home \
+  --publish 2375:2376 docker:dind
   
 docker rm -f jenkins
 docker container run --name jenkins --rm --detach \
@@ -17,4 +17,5 @@ docker container run --name jenkins --rm --detach \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --volume "$PWD"/jenkins-data:/var/jenkins_home \
   --volume "$PWD"/jenkins-docker-certs:/certs/client:ro \
+   -v "$PWD":/home \
   --publish 8080:8080 --publish 50000:50000 jenkinsci/blueocean
