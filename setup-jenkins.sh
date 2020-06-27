ee82f614bc1a7774e08ba41f1d752d6fceb381f5
codesenju <codesenju@gmail.com>
2020-06-27 12:45:40 +0200

    working script

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-mkdir -pv $PWD/jenkins-docker-certs
-mkdir -pv $PWD/jenkins-data
 docker rm -f jenkins-docker
-docker container run --name jenkins-docker --rm --detach \
+docker container run -u 0 --name jenkins-docker --rm -d \
   --privileged --network jenkins --network-alias docker \
-  --env DOCKER_TLS_CERTDIR=/certs \
-  --volume "$PWD"/jenkins-docker-certs:/certs/client \
-  --volume "$PWD"/jenkins-data:/var/jenkins_home \
-   -v "$PWD":/home \
-  --publish 2375:2376 docker:dind
+  -e DOCKER_TLS_CERTDIR=/certs \
+  -v /opt/jenkins/jenkins-docker-certs:/certs/client \
+  -v /opt/jenkins/jenkins-data:/var/jenkins_home \
+  -p 2375:2376 docker:dind
   
 docker rm -f jenkins
-docker container run --name jenkins --rm --detach \
-  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
-  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
-  --volume "$PWD"/jenkins-data:/var/jenkins_home \
-  --volume "$PWD"/jenkins-docker-certs:/certs/client:ro \
-   -v "$PWD":/home \
-  --publish 8080:8080 --publish 50000:50000 jenkinsci/blueocean
+docker container run -u 0 --name jenkins --rm -d \
+  --network jenkins -e DOCKER_HOST=tcp://docker:2376 \
+  -e DOCKER_CERT_PATH=/certs/client -e DOCKER_TLS_VERIFY=1 \
+  -v /opt/jenkins/jenkins-data:/var/jenkins_home \
+  -v /opt/jenkins/jenkins-docker-certs:/certs/client:ro \
+  -p 8080:8080 -p 50000:50000 jenkinsci/blueocean
